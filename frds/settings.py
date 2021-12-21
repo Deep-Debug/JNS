a22b0367de307db94e4cd9bf75d253de7e5719c5
Deep <62534255+Deep-Debug@users.noreply.github.com>
2021-12-21 23:22:31 +0530

    hide password

--- frds/settings.py
+++ frds/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 EMAIL_USE_TLS = True  
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587  
-EMAIL_HOST_USER = 'davebholu08@gmail.com'  
-EMAIL_HOST_PASSWORD = 'Dd@180820'  
+EMAIL_HOST_USER = '****@gmail.com'  
+EMAIL_HOST_PASSWORD = '****'  
 
 
 ALLOWED_HOSTS = []
